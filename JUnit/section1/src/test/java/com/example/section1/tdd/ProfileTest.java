0cbea6b816c429cd621a8073cefe0fd01ef3a190
rtg <rtg1539@naver.com>
2024-07-03 15:06:56 +0900

    자바와 JUnit을 활용한 실용주의 단위 테스트 12장 - TDD 기능 개선 및 테스트 리팩토링1

--- JUnit/section1/src/test/java/com/example/section1/tdd/ProfileTest.java
+++ JUnit/section1/src/test/java/com/example/section1/tdd/ProfileTest.java
@@ -1,18 +1,44 @@
 package com.example.section1.tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProfileTest {
+    private Profile profile;
+    private BooleanQuestion questionIsThereRelocation;
+    private Answer answerThereIsRelocation;
+
+    @BeforeEach
+    public void createProfile() {
+        profile = new Profile();
+    }
+
+
+    @BeforeEach
+    public void createQuestionAndAnswer() {
+        questionIsThereRelocation = new BooleanQuestion(1, "Relocation package");
+        answerThereIsRelocation = new Answer(questionIsThereRelocation, Bool.TRUE);
+    }
 
     @Test
     public void matchesNothingWhenProfileEmpty() {
-        Profile profile = new Profile();
-        Question question = new BooleanQuestion(1, "Relocation package");
-        Criterion criterion = new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare);
+        Criterion criterion = new Criterion(answerThereIsRelocation, Weight.DontCare);
+
         boolean result = profile.matches(criterion);
+
         assertFalse(result);
 
+    }
+
+    @Test
+    public void matchesWhenProfileContainsMatchingAnswer() {
+        profile.add(answerThereIsRelocation);
+        Criterion criterion = new Criterion(answerThereIsRelocation, Weight.Important);
+
+        boolean result = profile.matches(criterion);
+
+        assertTrue(result);
     }
 }
